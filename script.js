98434af8b55dcec97dd65765235f867dbccdc6c8
Travyynn Ippolito <ippolito3717@gmail.com>
2023-11-07 14:38:25 -0500

    appended names to main ol

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 const h2 = document.createElement('h2');
 const main = document.querySelector('main');
+const ol = document.createElement('ol');
 
 const getAllArtists = async () => {
   h2.innerText = 'Artists';
   main.appendChild(h2);
+  main.appendChild(ol);
   const response = await fetch('https://fsa-crud-2aa9294fe819.herokuapp.com/api/2310-FSA-ET-WEB-FT-SF/artists');
   const artistsInfo = await response.json();
+
   for (let i = 0; i < artistsInfo.data.length; i++) {
-    console.log(artistsInfo.data[i].name);
+    const li = document.createElement('li');
+    li.innerText = artistsInfo.data[i].name;
+    ol.appendChild(li);
   }
 }
 getAllArtists();
-
-
-// for (let i = 0; i < artistsInfo.data.length; i++) {
-//   console.log(artistsInfo.data[i].name);
-// }
